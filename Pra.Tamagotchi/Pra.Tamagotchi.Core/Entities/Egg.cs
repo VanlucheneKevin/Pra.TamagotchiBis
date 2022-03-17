cef1688887e9420489c429207bda7c62cfe7fa94
VanlucheneKevin <kevin.vanluchene@student.howest.be>
2022-03-17 11:20:25 +0100

    first commit

---
+++ Pra.Tamagotchi/Pra.Tamagotchi.Core/Entities/Egg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Pra.Tamagotchi.Core.Interfaces;
+using Pra.Tamagotchi.Core.Enums;
+
+namespace Pra.Tamagotchi.Core.Entities
+{
+    public class Egg : Tamagotchi, IHatchable
+    {
+        public Egg() : base()
+        {
+
+        }
+        public override void Grow()
+        {
+            Size++;
+        }
+
+        public Chick Hatch()
+        {
+            Chick chick = new Chick(Status);
+            if (Size < 3)
+            {
+                throw new InvalidOperationException("Ei is niet groot genoeg om uit te broeden");
+            }
+            else if( Size >= 3 && Size < 6)
+            {
+                Status = random.Next(2) == 0 ? TamagotchiStatus.Healthy : TamagotchiStatus.Sick;
+            }
+            else
+            {
+                Status = TamagotchiStatus.Healthy;
+            }
+            return chick ;
+        }
+
+        public override string ToString()
+        {
+            return $"Ei {Size:*}";
+        }
+    }
+}
